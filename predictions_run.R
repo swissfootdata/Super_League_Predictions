bff6703a5573e6b02320698a1ab885d200200be8
swissfootdata <swissfootballdata@bluewin.ch>
2024-08-13 10:19:26 +0200

    commit from Rstudio

--- predictions_run.R
+++ predictions_run.R
@@ -54,3 +54,24 @@ cat(round,file="rounds_played.txt")
 #Make Commit
 source("commit.R", encoding = "UTF-8")
 
+#Apdaptations
+upcoming_matches_all[132,] <- upcoming_matches_all[41,]
+upcoming_matches_all$team_away[132] <- upcoming_matches_all$team_away[40]
+upcoming_matches_all$team_away_ranking[132] <- upcoming_matches_all$team_away_ranking[40]
+upcoming_matches_all$threemonths_performance_away[132] <- upcoming_matches_all$threemonths_performance_away[40]
+upcoming_matches_all$year_performance_away[132] <- upcoming_matches_all$year_performance_away[40]
+upcoming_matches_all$threeyear_performance_away[132] <- upcoming_matches_all$threeyear_performance_away[40]
+upcoming_matches_all$elo_away[132] <- upcoming_matches_all$elo_away[40]
+upcoming_matches_all$mv_average_away[132] <-upcoming_matches_all$mv_average_away[40]
+upcoming_matches_all$round[132] <- 5
+
+upcoming_matches[108,] <- upcoming_matches[17,]
+upcoming_matches$team_away[108] <- upcoming_matches$team_away[16]
+upcoming_matches$team_away_ranking[108] <- upcoming_matches$team_away_ranking[16]
+upcoming_matches$threemonths_performance_away[108] <- upcoming_matches$threemonths_performance_away[16]
+upcoming_matches$year_performance_away[108] <- upcoming_matches$year_performance_away[16]
+upcoming_matches$threeyear_performance_away[108] <- upcoming_matches$threeyear_performance_away[16]
+upcoming_matches$elo_away[108] <- upcoming_matches$elo_away[16]
+upcoming_matches$mv_average_away[108] <-upcoming_matches$mv_average_away[16]
+upcoming_matches$round[108] <- 5
+
