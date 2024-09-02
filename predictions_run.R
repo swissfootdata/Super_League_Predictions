f026742707fbe875ac8f4b6a6a05f54116be4561
swissfootdata <swissfootballdata@bluewin.ch>
2024-09-02 09:08:29 +0200

    commit from Rstudio

--- predictions_run.R
+++ predictions_run.R
@@ -7,7 +7,7 @@ source("config.R")
 
 #Season
 season <- "24/25"
-
+View(data_transfermarkt_new)
 #Get Elo-Daten
 source("get_elodata.R", encoding = "UTF-8")
 
@@ -26,12 +26,12 @@ source("getting_ids.R", encoding = "UTF-8")
 rounds_played <- as.numeric(read.delim("rounds_played.txt", header=FALSE))
 
 #Adaptions
-#games <- c(4362927)
+#games <- c(4362929,4362930,4362931)
 #round <- 5
-new_matches <- new_matches[-1]
-missing_matches <- c()
-position_missing_matches <- c()
-#new_matches[(length(new_matches)+1):(length(new_matches)+length(missing_matches))] <- missing_matches
+#new_matches <- new_matches[-1]
+missing_matches <- c(4362928,4362932)
+position_missing_matches <- c(29,30)
+new_matches[(length(new_matches)+1):(length(new_matches)+length(missing_matches))] <- missing_matches
 
 #Scrape recently played matches
 source("get_new_data.R", encoding = "UTF-8")
@@ -54,24 +54,24 @@ cat(round,file="rounds_played.txt")
 #Make Commit
 source("commit.R", encoding = "UTF-8")
 
-#Apdaptations
-upcoming_matches_all[132,] <- upcoming_matches_all[41,]
-upcoming_matches_all$team_away[132] <- upcoming_matches_all$team_away[40]
-upcoming_matches_all$team_away_ranking[132] <- upcoming_matches_all$team_away_ranking[40]
-upcoming_matches_all$threemonths_performance_away[132] <- upcoming_matches_all$threemonths_performance_away[40]
-upcoming_matches_all$year_performance_away[132] <- upcoming_matches_all$year_performance_away[40]
-upcoming_matches_all$threeyear_performance_away[132] <- upcoming_matches_all$threeyear_performance_away[40]
-upcoming_matches_all$elo_away[132] <- upcoming_matches_all$elo_away[40]
-upcoming_matches_all$mv_average_away[132] <-upcoming_matches_all$mv_average_away[40]
-upcoming_matches_all$round[132] <- 5
-
-upcoming_matches[108,] <- upcoming_matches[17,]
-upcoming_matches$team_away[108] <- upcoming_matches$team_away[16]
-upcoming_matches$team_away_ranking[108] <- upcoming_matches$team_away_ranking[16]
-upcoming_matches$threemonths_performance_away[108] <- upcoming_matches$threemonths_performance_away[16]
-upcoming_matches$year_performance_away[108] <- upcoming_matches$year_performance_away[16]
-upcoming_matches$threeyear_performance_away[108] <- upcoming_matches$threeyear_performance_away[16]
-upcoming_matches$elo_away[108] <- upcoming_matches$elo_away[16]
-upcoming_matches$mv_average_away[108] <-upcoming_matches$mv_average_away[16]
-upcoming_matches$round[108] <- 5
+ #Apdaptations
+# upcoming_matches_all[132,] <- upcoming_matches_all[41,]
+# upcoming_matches_all$team_away[132] <- upcoming_matches_all$team_away[40]
+# upcoming_matches_all$team_away_ranking[132] <- upcoming_matches_all$team_away_ranking[40]
+# upcoming_matches_all$threemonths_performance_away[132] <- upcoming_matches_all$threemonths_performance_away[40]
+# upcoming_matches_all$year_performance_away[132] <- upcoming_matches_all$year_performance_away[40]
+# upcoming_matches_all$threeyear_performance_away[132] <- upcoming_matches_all$threeyear_performance_away[40]
+# upcoming_matches_all$elo_away[132] <- upcoming_matches_all$elo_away[40]
+# upcoming_matches_all$mv_average_away[132] <-upcoming_matches_all$mv_average_away[40]
+# upcoming_matches_all$round[132] <- 5
+# 
+# upcoming_matches[108,] <- upcoming_matches[17,]
+# upcoming_matches$team_away[108] <- upcoming_matches$team_away[16]
+# upcoming_matches$team_away_ranking[108] <- upcoming_matches$team_away_ranking[16]
+# upcoming_matches$threemonths_performance_away[108] <- upcoming_matches$threemonths_performance_away[16]
+# upcoming_matches$year_performance_away[108] <- upcoming_matches$year_performance_away[16]
+# upcoming_matches$threeyear_performance_away[108] <- upcoming_matches$threeyear_performance_away[16]
+# upcoming_matches$elo_away[108] <- upcoming_matches$elo_away[16]
+# upcoming_matches$mv_average_away[108] <-upcoming_matches$mv_average_away[16]
+# upcoming_matches$round[108] <- 5
 
